d420babad0eb6619d6ddfabcfce3502f91f5a4b5
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-10-21 18:54:59 -0300

    Refactor Mensagens class and add game mode method

--- Mensagens.java
+++ Mensagens.java
@@ -1,21 +1,18 @@
-//package tartaruga;
+
 
 public class Mensagens {
 
     public static void boasVindas() {
         System.out.println("====== Bem vindo ao sistema do robô! ======");
     }
+    public static void modoDeJogo() {
+        System.out.println("Escolha o modo de jogo: \n 1) 1 robô controlado pelo usuário \n 2) 2 robôs movidos randomicamente");
+    }
     public static void perguntaCorRobo() {
         System.out.println("Qual a cor do seu robô?");
-    }
-    public static void perguntaCorRoboInteligente() {
-        System.out.println("Qual a cor do robô inteligente?");
-    }
-    public static void perguntaPosicaoXFruta() {
-        System.out.println("Qual a posição X da fruta?");
-    }
-    public static void perguntaPosicaoYFruta() {
-        System.out.println("Qual a posição Y da fruta?");
+    } 
+    public static void posicaoFruta(int frutaX, int frutaY) {
+        System.out.println("A fruta foi posicionada na coordenada: (" + frutaX + ", " + frutaY + ")");
     }
     public static void posicaoFrutaInvalida() {
         System.out.println("Posição inválida! A fruta deve estar entre 0 e 10 em ambos os eixos.");
@@ -26,4 +23,5 @@ public class Mensagens {
     public static void posicaoRoboAtual(int x, int y) {
         System.out.println("A posição atual do robô é: (" + x + ", " + y + ")");
     }
+
 }
