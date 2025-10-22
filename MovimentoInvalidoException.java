9afc882c386ed694ea2c50b4e0b7139a1232fd32
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-10-21 22:24:38 -0300

    Update MovimentoInvalidoException.java

--- MovimentoInvalidoException.java
+++ MovimentoInvalidoException.java
@@ -1,21 +1,12 @@
 /*pra falar a verdade, não vi direito a parte de tratamento de exceçoes
  ainda, mas se tiver algo errado meu fi ajeita.*/
 
-//package tartaruga;
-
 public class MovimentoInvalidoException extends Exception{
 	private static final long serialVersionUID = 8;
 	
-	public MovimentoInvalidoException(String aviso) { //recebe uma string para retorar na exception;
-		super(aviso);
-	}
+	public MovimentoInvalidoException(String aviso) {super(aviso);}
 	
-	public MovimentoInvalidoException() {
-		super("Movimento invalido, pois posição irá ser negativa.");
-	}
+	public MovimentoInvalidoException() {super(" Movimento invalido, pois a posição irá ser negativa.");}
 	
-	public String toString() {
-		return "movimento invalido.";
-	}
-	
+	public String toString() {return "movimento invalido.";}
 }
