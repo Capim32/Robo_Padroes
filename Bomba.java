5433cf203be36aeac87239ef7abf7f157bea16c9
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-10-21 22:21:34 -0300

    Refactor Bomba class with explosion handling
    Updated Bomba class to include explosion logic and a constant symbol.

--- Bomba.java
+++ Bomba.java
@@ -1,11 +1,17 @@
 public class Bomba extends Obstaculo {
+    public static final String SIMBOLO = "B";
 
     public Bomba(int id, int x, int y) {super(id, x, y);}
 
     @Override
-    public void colisao(Robo robo) throws MovimentoInvalidoException {
-        throw new MovimentoInvalidoException("O robô colidiu com uma bomba na posição (" + this.getX() + ", " + this.getY() + ") e não pode se mover nesta rodada.");
+    public void bater(Robo robo, Tabuleiro tab, int oldX, int oldY) {
+        if (!robo.isExplodiu()) { // Garante que só explode uma vez
+            robo.setExplodiu(true);
+            System.out.println("!!! AVISO: O Robô " + robo.getCorRobo() + " explodiu na Bomba (" + getX() + "," + getY() + ") e não pode mais se mover! !!!");
+            tab.removerObstaculo(this); // a bomba some do tabuleiro
+        }
     }
+
     @Override
-    public String getSimbolo() {return "X";}
+    public String getSimbolo() {return SIMBOLO;}
 }
