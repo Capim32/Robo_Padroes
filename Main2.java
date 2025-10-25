1b9073e14f3257020601a8f6d379078f10cb4f2c
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-10-24 22:51:37 -0300

    main 2 concluída por hora
    falta: arrumar a legenda, adicionar cor (se possivel) e corrigir a sobreposicao de robos (eles ocupam a mesma posicao, o movimento é invalidado mas é concluído). boa noite cristopher, amanhã vejo isso

--- Main2.java
+++ Main2.java
@@ -1,20 +1,25 @@
 
-import java.util.InputMismatchException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main2 {
-    private static final int TAMANHO_MAPA = 5;
-    Random random = new Random();
-
+    private static final int TAMANHO_MAPA = 4;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        Robo robo1 = new Robo("vermelho");
+        Random random = new Random();
+        
+        Tabuleiro tabuleiro = new Tabuleiro();
+        Robo robo1 = new Robo("Vermelho");
         Robo robo2 = new Robo("Azul");
+        List<Robo> robos = Arrays.asList(robo1, robo2);
 
-        System.out.println("Dois robôs inicializados: " + robo1.getCorRobo() + " (V) e " + robo2.getCorRobo() + " (A).");
+        Mensagens.boasVindas();
+        System.out.println("Main 2: 2 robôs randômicos");
+        System.out.println("Ambos os robôs foram iniciados nas coordenadas (0,0).");
 
+        /* 
         int frutaX = -1, frutaY = -1;
         while (frutaX < 0 || frutaY < 0 || frutaX >= TAMANHO_MAPA || frutaY >= TAMANHO_MAPA) {
             try {
@@ -29,12 +34,53 @@ public class Main2 {
             }
 
         }
+        */
+        int frutaX = Tabuleiro.obterCoordenadaValida(scanner, "coordenada X da fruta: ");
+        int frutaY = Tabuleiro.obterCoordenadaValida(scanner, "coordenada Y da fruta: ");
+        tabuleiro.setFruta(frutaX, frutaY);
         System.out.println("a fruta foi posicionada na coordenada: (" + frutaX + ", " + frutaY + ")");
-        
+
+        System.out.println("pressione enter para continuar...");
+        scanner.nextLine();
+
         // loop principal do jogo
+        Robo vencedor = null;
+        int rodada = 0;
+        while (vencedor == null) {
+            rodada++ ;
+            
+            Robo roboAtual = robos.get(rodada %2);
 
+            int oldX = roboAtual.getX();
+            int oldY = roboAtual.getY();
 
+            int movimentoRand = random.nextInt(4) + 1;
+
+            try {
+                roboAtual.mover(movimentoRand);
+                tabuleiro.verificarColisao(roboAtual, oldX, oldY);
+                tabuleiro.imprimir(robos);
+
+                if (roboAtual.seAlimentou(frutaX, frutaY)) {vencedor = roboAtual;}
+            } catch (MovimentoInvalidoException e) {}
+            
+            if (vencedor != null) {break;}
+
+            Tabuleiro.pausar(500);            
+        }
+
+    System.out.println("\n--- Fim do Cenário 2 ---");
+        System.out.println("O Robô VENCEDOR é o " + vencedor.getCorRobo() + "!");
+        System.out.println("Ele encontrou o alimento em (" + vencedor.getX() + ", " + vencedor.getY() + ").");
+        System.out.println("------------------------------------");
         
+        for (Robo r : robos) {
+            System.out.println("Robô " + r.getCorRobo() +": ");
+            System.out.println("   -> Movimentos Válidos: " + r.getMovimentosValidos());
+            System.out.println("   -> Movimentos Inválidos: " + r.getMovimentosInvalidos());
+        }
 
-    }
+        scanner.close();        
+        
+    }    
 }
