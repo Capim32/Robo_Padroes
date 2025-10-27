e0e90f60914187c96b228cd6f8f322e26c589f35
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-10-27 20:41:02 -0300

    Improve robot color selection and output formatting
    Refactor robot color selection and output messages.

--- Main3.java
+++ Main3.java
@@ -9,11 +9,55 @@ public class Main3 {
         Random random = new Random();
 
         Tabuleiro tabuleiro = new Tabuleiro();
-        Robo roboNormal = new Robo("azul");
-        RoboInteligente roboInteligente = new RoboInteligente("vermelho");
-        List<Robo> robos = Arrays.asList(roboNormal,roboInteligente);
+        String corRobo1 = "", corRobo2 = "";
+        int cor1, cor2;
 
-        System.out.println("cenário 3: Robô normal vs Robô inteligente");
+        Mensagens.boasVindas();
+
+        // infelizmente lembrei de fazer isso de ultima hora, tá meio porco
+        do {
+            Mensagens.perguntaCorRobo();
+            System.out.print("robô Normal: ");
+            cor1 = scanner.nextInt();
+            if (cor1 > 0 && cor1 < 5) {
+                switch (cor1) {
+                    case 1: corRobo1 = "VERMELHO"; break;
+                    case 2: corRobo1 = "AZUL"; break;
+                    case 3: corRobo1 = "AMARELO"; break;
+                    case 4: corRobo1 = "VERDE"; break;
+                    default: System.out.println("código de cor válido");break;
+                }
+            }
+            else {System.out.println("código de cor inválido, tente novamente");}
+            
+            
+        } while (cor1 <1 || cor1 > 4);
+
+        do {
+            Mensagens.perguntaCorRobo();
+            System.out.print("robô Inteligente: ");
+            cor2 = scanner.nextInt();
+            if (cor2 > 0 && cor2 < 5) {
+                switch (cor2) {
+                    case 1: corRobo2 = "VERMELHO"; break;
+                    case 2: corRobo2 = "AZUL"; break;
+                    case 3: corRobo2 = "AMARELO"; break;
+                    case 4: corRobo2 = "VERDE"; break;
+                    default: System.out.println("código de cor válido");break;
+                }
+            }
+            else {System.out.println("código de cor inválido, tente novamente");}
+            
+            
+        } while (cor2 <1 || cor2 > 4);
+
+        Robo roboNormal = new Robo(corRobo1);
+        Robo roboInteligente = new Robo(corRobo2);
+        List<Robo> robos = Arrays.asList(roboNormal, roboInteligente);
+        tabuleiro.adicionarRobo(roboNormal);
+        tabuleiro.adicionarRobo(roboInteligente);
+
+        System.out.println("Main 3: Robô normal vs Robô inteligente");
 
         int frutaX = Tabuleiro.obterCoordenadaValida(scanner, "Digite a coordenada X da fruta");
         int frutaY = Tabuleiro.obterCoordenadaValida(scanner, "Digite a coordenada Y da fruta");
@@ -47,22 +91,23 @@ public class Main3 {
                 
                 // não há colisão nesse cenário, mas para manter a uniformidade do código...
                 tabuleiro.verificarColisao(roboAtual, oldX, oldY);
+                tabuleiro.isPosicaoOcupadaPorRobo(roboAtual, oldX, oldY);
 
                 tabuleiro.imprimir(robos);
 
                 if (roboAtual.seAlimentou(frutaX, frutaY)) {
                     if (roboAtual == roboNormal) {
                         rNaoAchou = false;
-                        System.out.println("robô normal encontrou a fruta!");
+                        System.out.println(Robo.ANSI_GREEN + "robô normal encontrou a fruta!" + Robo.ANSI_RESET);
                     }
                     else if (roboAtual == roboInteligente) {
                         iNaoAchou = false;
-                        System.out.println("robô inteligente encontrou a fruta!");
+                        System.out.println(Robo.ANSI_GREEN + "robô inteligente encontrou a fruta!" + Robo.ANSI_RESET);
                     }
                 }
             }
 
-            Tabuleiro.pausar(1000);
+            Tabuleiro.pausar(500);
         }
 
         System.out.println("fim do jogo!");
@@ -70,13 +115,13 @@ public class Main3 {
         int totalMovNormal = roboNormal.getMovimentosInvalidos() + roboNormal.getMovimentosInvalidos();
         int totalMovInteligente = roboInteligente.getMovimentosInvalidos() + roboInteligente.getMovimentosInvalidos();
 
-        System.out.println("Robô Normal (" + roboNormal.getCorRobo() + ") Total de Tentativas (V+I): " + totalMovNormal);
-        System.out.println("Robô Inteligente (" + roboInteligente.getCorRobo() + ") Total de Tentativas (V+I): " + totalMovInteligente);
+        System.out.println("Robô Normal (" + roboNormal.ANSI_COR + roboNormal.getCorRobo() + roboNormal.ANSI_RESET +") Total de Tentativas (V+I): " + totalMovNormal);
+        System.out.println("Robô Inteligente (" + roboInteligente.ANSI_COR + roboInteligente.getCorRobo() + roboInteligente.ANSI_RESET +") Total de Tentativas (V+I): " + totalMovInteligente);
 
         if (totalMovNormal < totalMovInteligente) {
-            System.out.println("O Robô Normal foi o mais eficiente (menor número de tentativas).");
+            System.out.println(Robo.ANSI_GREEN + "O Robô Normal foi o mais eficiente (menor número de tentativas)." + Robo.ANSI_RESET);
         } else if (totalMovInteligente < totalMovNormal) {
-            System.out.println("O Robô Inteligente foi o mais eficiente (menor número de tentativas).");
+            System.out.println(Robo.ANSI_GREEN + "O Robô Inteligente foi o mais eficiente (menor número de tentativas)." + Robo.ANSI_RESET);
         } else {
             System.out.println("Ambos empataram em número de tentativas.");
         }
