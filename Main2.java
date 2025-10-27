aaf57016623d025c213a000429153faef0941531
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-10-26 21:30:23 -0300

    Refactor Main2.java for improved readability

--- Main2.java
+++ Main2.java
@@ -11,34 +11,20 @@ public class Main2 {
         Random random = new Random();
         
         Tabuleiro tabuleiro = new Tabuleiro();
-        Robo robo1 = new Robo("Vermelho");
-        Robo robo2 = new Robo("Azul");
+        Robo robo1 = new Robo("VERMELHO");
+        Robo robo2 = new Robo("AZUL");
         List<Robo> robos = Arrays.asList(robo1, robo2);
 
         Mensagens.boasVindas();
+        
         System.out.println("Main 2: 2 robôs randômicos");
-        System.out.println("Ambos os robôs foram iniciados nas coordenadas (0,0).");
+        System.out.println( Robo.ANSI_BLUE + "Ambos os robôs foram iniciados nas coordenadas (0,0)." + Robo.ANSI_RESET);
 
-        /* 
-        int frutaX = -1, frutaY = -1;
-        while (frutaX < 0 || frutaY < 0 || frutaX >= TAMANHO_MAPA || frutaY >= TAMANHO_MAPA) {
-            try {
-                System.out.print("\nDefina a posição X do alimento (0 a 4): ");
-                frutaX = scanner.nextInt();
-                System.out.print("Defina a posição Y do alimento (0 a 4): ");
-                frutaY = scanner.nextInt();
-                scanner.nextLine();
-            } catch (InputMismatchException e) {
-                System.err.println("Entrada inválida. Digite apenas números inteiros.");
-                scanner.nextLine();
-            }
 
-        }
-        */
         int frutaX = Tabuleiro.obterCoordenadaValida(scanner, "coordenada X da fruta: ");
         int frutaY = Tabuleiro.obterCoordenadaValida(scanner, "coordenada Y da fruta: ");
         tabuleiro.setFruta(frutaX, frutaY);
-        System.out.println("a fruta foi posicionada na coordenada: (" + frutaX + ", " + frutaY + ")");
+        System.out.println(Robo.ANSI_GREEN + "a fruta foi posicionada na coordenada: (" + frutaX + ", " + frutaY + ")" + Robo.ANSI_RESET);
 
         System.out.println("pressione enter para continuar...");
         scanner.nextLine();
@@ -66,16 +52,16 @@ public class Main2 {
             
             if (vencedor != null) {break;}
 
-            Tabuleiro.pausar(500);            
+            Tabuleiro.pausar(1000);            
         }
 
     System.out.println("\n--- Fim do Cenário 2 ---");
-        System.out.println("O Robô VENCEDOR é o " + vencedor.getCorRobo() + "!");
+        System.out.println( Robo.ANSI_GREEN +"O Robô VENCEDOR é o " + vencedor.getCorRobo() + Robo.ANSI_RESET + "!");
         System.out.println("Ele encontrou o alimento em (" + vencedor.getX() + ", " + vencedor.getY() + ").");
         System.out.println("------------------------------------");
         
         for (Robo r : robos) {
-            System.out.println("Robô " + r.getCorRobo() +": ");
+            System.out.println("Robô " + r.ANSI_COR +  r.getCorRobo() + r.ANSI_RESET +": ");
             System.out.println("   -> Movimentos Válidos: " + r.getMovimentosValidos());
             System.out.println("   -> Movimentos Inválidos: " + r.getMovimentosInvalidos());
         }
